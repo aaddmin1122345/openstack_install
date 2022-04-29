aea5a447b7d92d41621b0b14347b4c55eb7bbf9f
qxqzx <2443223489@qq.com>
2022-04-29 16:20:03 +0800

    “格式化代码”

--- README.md
+++ README.md
@@ -1,7 +1,4 @@
-# openstack_install
-脚本主要适用于昆明冶专信息安全2001班教学使用,其他环境下可能并不适用此脚本
-
-使用方法1:
+# 使用方法1:
 
 1   git clone https://github.com/qxqzx3489/openstack_install
 
@@ -14,7 +11,7 @@
 5   ./openstack_install.sh 或者sh openstack_install.sh
 
 
-使用方法2:
+# 使用方法2:
 
 找到右边的Releases,把压缩包下载下来,然后直接运行脚本即可
 
@@ -23,4 +20,6 @@
 问题:
 
 安装遇到问题请带上日志反馈 日志存放在$HOME/openstack_install.log
+
+
 
--- openstack_install.sh
+++ openstack_install.sh
@@ -1,23 +1,21 @@
 #! /usr/bin/bash
 rm -rf $HOME/openstack_install.log
-log(){
-daojishi(){
-    a=$1
-    b=$2
-    ti1=`date +%s`
-    ti2=`date +%s`
-    i=$(($ti2 - $ti1 ))
-    echo -e "安装脚本将在$a秒之后开始运行!
+log() {
+	daojishi() {
+		a=$1
+		b=$2
+		ti1=$(date +%s)
+		ti2=$(date +%s)
+		i=$(($ti2 - $ti1))
+		echo -e "安装脚本将在$a秒之后开始运行!
     认真阅读使用须知!\n"
-    echo -e $b
-    while [[ "$i" -ne "$a" ]]
-    do
-	ti2=`date +%s`
-	i=$(($ti2 - $ti1 ))
-    done
-
-}
-daojishi 60 '使用须知:\n
+		echo -e $b
+		while [[ "$i" -ne "$a" ]]; do
+			ti2=$(date +%s)
+			i=$(($ti2 - $ti1))
+		done
+	} #暂停时间
+	daojishi 60 '使用须知:\n
 \033[1;91m必须先手动把静态ip配置好,不然安装后ip变了可能就没了\n
 虚拟机系统版本必须为centos8\n
 虚拟机网卡需要必须选择为nat网卡\n
@@ -29,49 +27,51 @@ daojishi 60 '使用须知:\n
 上述操作都没问题了,就等待安装
 如要取消安装请按ctrl+c\n'
 
-echo -e "\033[1;92m开始安装..."
-echo -e "\033[1;92m配置selinux中..."
-echo "/usr/sbin/setenforce 0" >> /etc/rc.local
-echo -e "\033[1;92m关闭防火墙中..."
-systemctl disable firewalld && systemctl stop firewalld
-echo -e "LANG=en_US.utf-8 \nLC_ALL=en_US.utf-8" >/etc/environment
-lip=$(ifconfig ens33|grep inet|grep netmask|awk '{print$2}')
-echo -e "\033[1;92m你的ip是:$lip"
-echo -e "\033[1;92m设置主机名中..."
-hostnamectl set-hostname openstack
-echo -e "\033[1;92m更改hosts文件中..."
-echo -e "$lip\topenstack\topenstack.localdomain" >>/etc/hosts
-hosts=$(cat /etc/hosts)
-echo "修改后的hosts文件内容为:\n
+	echo -e "\033[1;92m开始安装..."
+	echo -e "\033[1;92m配置selinux中..."
+	echo "/usr/sbin/setenforce 0" >>/etc/rc.local
+	echo -e "\033[1;92m关闭防火墙中..."
+	systemctl disable firewalld && systemctl stop firewalld
+	echo -e "LANG=en_US.utf-8 \nLC_ALL=en_US.utf-8" >/etc/environment
+	lip=$(ifconfig ens33 | grep inet | grep netmask | awk '{print$2}')
+	echo -e "\033[1;92m你的ip是:$lip"
+	echo -e "\033[1;92m设置主机名中..."
+	hostnamectl set-hostname openstack
+	echo -e "\033[1;92m更改hosts文件中..."
+	echo -e "$lip\topenstack\topenstack.localdomain" >>/etc/hosts
+	hosts=$(cat /etc/hosts)
+	echo "修改后的hosts文件内容为:\n
 $hosts"
-echo -e "\033[1;92m更换软件源为中科大源中..."
-sed -e 's|^mirrorlist=|#mirrorlist=|g' \
-         -e 's|^#baseurl=http://mirror.centos.org/$contentdir|baseurl=https://mirrors.ustc.edu.cn/centos|g' \
-         -i.bak \
-         /etc/yum.repos.d/CentOS-Linux-AppStream.repo \
-         /etc/yum.repos.d/CentOS-Linux-BaseOS.repo \
-         /etc/yum.repos.d/CentOS-Linux-Extras.repo \
-         /etc/yum.repos.d/CentOS-Linux-PowerTools.repo \
-         /etc/yum.repos.d/CentOS-Linux-Plus.repo
-echo -e "\033[1;92m刷新源并切换默认网络管理为network..."
-yum makecache;dnf update -y;dnf install network-scripts -y
-systemctl stop NetworkManager && systemctl disable NetworkManager
-systemctl enable --now network
-echo -e "\033[1;92m安装centos-release-openstack-train中..."
-dnf install centos-release-openstack-train -y
-dnf config-manager --enable powertools
-dnf install openstack-packstack -y
-dnf update -y
-daojishi 20 "$a秒后开始最后的安装
+	echo -e "\033[1;92m更换软件源为中科大源中..."
+	sed -e 's|^mirrorlist=|#mirrorlist=|g' \
+	-e 's|^#baseurl=http://mirror.centos.org/$contentdir|baseurl=https://mirrors.ustc.edu.cn/centos|g' \
+	-i.bak \
+	/etc/yum.repos.d/CentOS-Linux-AppStream.repo \
+	/etc/yum.repos.d/CentOS-Linux-BaseOS.repo \
+	/etc/yum.repos.d/CentOS-Linux-Extras.repo \
+	/etc/yum.repos.d/CentOS-Linux-PowerTools.repo \
+	/etc/yum.repos.d/CentOS-Linux-Plus.repo
+	echo -e "\033[1;92m刷新源并切换默认网络管理为network..."
+	yum makecache
+	dnf update -y
+	dnf install network-scripts -y
+	systemctl stop NetworkManager && systemctl disable NetworkManager
+	systemctl enable --now network
+	echo -e "\033[1;92m安装centos-release-openstack-train中..."
+	dnf install centos-release-openstack-train -y
+	dnf config-manager --enable powertools
+	dnf install openstack-packstack -y
+	dnf update -y
+	daojishi 20 "$a秒后开始最后的安装
     安装速度具体取决于你电脑性能"
-echo -e "\033[1;92m安装开始...\n
+	echo -e "\033[1;92m安装开始...\n
     "
-packstack --allinone;
-passwd=$(cat $HOME/keystonerc_admin|grep -v "OS_USERNAME="|grep "OS_PASSWORD=")
-echo $passwd| awk -F "'" '{print "你的登陆帐号为:admin\n你的登录密码为:"$2}'
-echo -e "\033[1;92m你的登陆地址是: $lip/dashboard"
-echo -e "\033[1;92m如果出现登陆地址以及帐号密码就说明安装成功啦~~~"
-echo -e "\033[36mgithub:https://github.com/qxqzx3489/openstack_install\n个人博客:https://qxqzx.xyz\n网站打不开请更换电脑dns为阿里巴巴提供的:223.5.5.5/223.6.6.6"
+	packstack --allinone
+	passwd=$(cat $HOME/keystonerc_admin | grep -v "OS_USERNAME=" | grep "OS_PASSWORD=")
+	echo $passwd | awk -F "'" '{print "你的登陆帐号为:admin\n你的登录密码为:"$2}'
+	echo -e "\033[1;92m你的登陆地址是: $lip/dashboard"
+	echo -e "\033[1;92m如果出现登陆地址以及帐号密码就说明安装成功啦~~~"
+	echo -e "\033[36mgithub:https://github.com/qxqzx3489/openstack_install\n个人博客:https://qxqzx.xyz\n网站打不开请更换电脑dns为阿里巴巴提供的:223.5.5.5/223.6.6.6"
 }
 
-log |tee -a $HOME/openstack_install.log
+log | tee -a $HOME/openstack_install.log
